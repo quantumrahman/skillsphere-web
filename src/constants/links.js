3575c0775f0fd6b1365e7f6629a420f2249c9440
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-11 13:53:16 +0600

    feat: create links and implement header links array of object

---
+++ src/constants/links.js
@@ -0,0 +1,17 @@
+export const headerLinks = [
+    {
+        id: 1,
+        path: '/',
+        lable: 'Home'
+    },
+    {
+        id: 2,
+        path: '/courses',
+        lable: 'Courses'
+    },
+    {
+        id: 3,
+        path: '/profile',
+        lable: 'Profile'
+    },
+];
