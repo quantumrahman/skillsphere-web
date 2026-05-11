a4ebbf946be77fc608d8622eb02a78a92ba8e49c
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-11 21:07:28 +0600

    feat: implement social and footer links with icons

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,3 +1,9 @@
+import { 
+    RiFacebookFill, 
+    RiLinkedinFill, 
+    RiTwitterFill 
+} from "@remixicon/react";
+
 export const headerLinks = [
     {
         id: 1,
@@ -13,5 +19,46 @@ export const headerLinks = [
         id: 3,
         path: '/profile',
         lable: 'Profile'
+    },
+];
+
+export const footerLinks = [
+    {
+        id: 1,
+        path: '/',
+        lable: 'Home'
+    },
+    {
+        id: 2,
+        path: '/courses',
+        lable: 'Courses'
+    },
+    {
+        id: 3,
+        path: '/',
+        lable: 'Terms & Conditions'
+    },
+    {
+        id: 4,
+        path: '/',
+        lable: 'Privacy policy'
+    },
+];
+
+export const socialLinks = [
+    {
+        id: 1,
+        path: '/',
+        icon: <RiFacebookFill size={16} className="text-[#121212]" />
+    },
+    {
+        id: 2,
+        path: '/',
+        icon: <RiLinkedinFill size={16} className="text-[#121212]" />
+    },
+    {
+        id: 3,
+        path: '/',
+        icon: <RiTwitterFill size={16} className="text-[#121212]" />
     },
 ];
