c80824768a9f1531c570ed19dd595e4d3c2458ab
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-12 13:41:27 +0600

    feat: create level badge and finalize design

---
+++ src/components/ui/Badges/LevelBadge/LevelBadge.jsx
@@ -0,0 +1,16 @@
+const LevelBadges = ({ level }) => {
+
+    const levelStyles = {
+        beginner: "bg-emerald-500/10 border-emerald-500/30 text-emerald-400",
+        intermediate: "bg-amber-500/10 border-amber-500/30 text-amber-400",
+        advanced: "bg-rose-500/10 border-rose-500/30 text-rose-400",
+    };
+
+    return (
+        <div className={`px-2 py-0.5 backdrop-blur-md rounded-full border ${levelStyles[level]}`}>
+            <span className="text-xs font-normal">{level}</span>
+        </div>
+)
+};
+
+export default LevelBadges;
--- src/components/ui/Cards/CourseCard/CourseCard.jsx
+++ src/components/ui/Cards/CourseCard/CourseCard.jsx
@@ -4,31 +4,32 @@ import {
 } from "@remixicon/react";
 
 import Image from "next/image";
+import LevelBadges from "../../Badges/LevelBadge/LevelBadge";
 
 const CourseCard = ({ course }) => {
     return (
         <div className="w-auto border border-[#2a2a2a] rounded-lg group">
             <div className="w-full h-[180px] sm:h-[250px] relative overflow-hidden rounded-lg">
                 <Image 
-                    src={'/images/postgre_sql_performance_tuning.png'}
+                    src={course?.image}
                     alt="postgre_sql_performance_tuning"
                     fill={true}
                     className="object-cover w-full h-full transition-all duration-500 ease-in-out grayscale group-hover:grayscale-0 group-hover:scale-110 group-hover:-rotate-3"
                 />
-                <div className="px-2 py-0.5 bg-indigo-500/15 backdrop-blur-md rounded-full absolute top-[18px] right-[18px] border border-indigo-500/30">
-                    <span className="text-xs font-normal text-indigo-400">Database</span>
+                <div className="w-fit absolute top-[18px] right-[18px]">
+                    <LevelBadges level={course?.level.toLowerCase()}/>
                 </div>
             </div>
             <div className="w-full px-6 py-4 space-y-0.5">
-                <h3 className="text-lg font-medium text-[#ffffff]">PostgreSQL Performance Tuning</h3>
+                <h3 className="text-lg font-medium text-[#ffffff]">{course?.title}</h3>
                 <div className="w-full flex items-center justify-between">
-                    <span className="text-sm font-normal text-[#8e8e8e]">Rakibul Rahman</span>
+                    <span className="text-sm font-normal text-[#8e8e8e]">{course?.instructor}</span>
                     <div className="flex items-center justify-center gap-2">
                         <RiStarSFill 
                             size={14} 
                             color="#8e8e8e" 
                         />
-                        <span className="text-sm font-normal text-[#8e8e8e]">4.9</span>
+                        <span className="text-sm font-normal text-[#8e8e8e]">{course?.rating}</span>
                     </div>
                 </div>
                 <button className="w-fit mx-auto mt-3 px-3 py-2 text-xs font-normal text-[#ffffff] flex items-center justify-center gap-1 cursor-pointer">
