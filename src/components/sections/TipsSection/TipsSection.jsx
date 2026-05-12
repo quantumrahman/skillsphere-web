8f0ae361b3d3dfb255e66a5226878cd6db327d01
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-12 19:28:54 +0600

    feat: implement dynamic data in tip section

--- src/components/sections/TipsSection/TipsSection.jsx
+++ src/components/sections/TipsSection/TipsSection.jsx
@@ -1,7 +1,9 @@
 import TipCard from "@/components/ui/Cards/TipCard/TipCard";
-import { RiAlarmLine } from "@remixicon/react";
+import { getTips } from "@/lib/get.tips";
 
-const TipsSection = () => {
+const TipsSection = async () => {
+    const tips = await getTips();
+
     return (
         <div className="w-full py-12 border-y border-[#2a2a2a] md:py-20">
             <div className="w-full max-w-[1400px] mx-auto">
@@ -10,12 +12,9 @@ const TipsSection = () => {
                     <p className="text-base font-normal text-[#8e8e8e] leading-relaxed sm:text-xl">Master new skills faster with our expert-backed learning strategies.</p>
                 </div>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                    <TipCard />
-                    <TipCard />
-                    <TipCard />
-                    <TipCard />
-                    <TipCard />
-                    <TipCard />
+                    {tips.map((tip) => (
+                        <TipCard key={tip.id} tip={tip}/>
+                    ))}
                 </div>
             </div>
         </div>
--- src/components/ui/Cards/TipCard/TipCard.jsx
+++ src/components/ui/Cards/TipCard/TipCard.jsx
@@ -1,4 +1,11 @@
-import { RiAlarmLine, RiBrainLine, RiCalendarLine, RiGitRepositoryLine, RiLightbulbLine, RiLineChartLine } from "@remixicon/react";
+import { 
+    RiAlarmLine, 
+    RiBrainLine, 
+    RiCalendarLine, 
+    RiGitRepositoryLine, 
+    RiLightbulbLine, 
+    RiLineChartLine 
+} from "@remixicon/react";
 
 const TipCard = ({ tip }) => {
     const iconsVariant = {
@@ -14,11 +21,11 @@ const TipCard = ({ tip }) => {
         <div className="w-full min-h-[50px] bg-transparent border border-[#2a2a2a] rounded-lg p-1">
             <div className="w-full h-full bg-linear-to-t from-transparent to-[#2a2a2a] rounded-md p-4">
                 <div className="w-full mb-6 sm:mb-[75px] md:mb-[55px]">
-                    
+                    {iconsVariant[tip?.icon]}
                 </div>
                 <div className="w-full flex flex-col gap-1 md:gap-2 flex-initial">
-                    <h3 className="text-base font-semibold text-[#ffffff] md:text-xl">Pomodoro Technique</h3>
-                    <p className="text-sm font-normal text-[#8e8e8e]">A method of studying for 25 minutes followed by a 5-minute break to improve focus and reduce fatigue.</p>
+                    <h3 className="text-base font-semibold text-[#ffffff] md:text-xl">{tip?.title}</h3>
+                    <p className="text-sm font-normal text-[#8e8e8e]">{tip?.description}</p>
                 </div>
             </div>
         </div>
