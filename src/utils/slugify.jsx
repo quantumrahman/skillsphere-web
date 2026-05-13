c6ae09ac5cf2deaff7589511dd435123aa816745
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-13 15:01:53 +0600

    feat: create slugify function

---
+++ src/utils/slugify.jsx
@@ -0,0 +1,9 @@
+export const slugify = (slug) => {
+    return slug
+        .toLowerCase()
+        .trim()
+        .replace(/&/g, "and")
+        .replace(/[^a-z0-9\s-]/g, "")
+        .replace(/\s+/g, "-")
+        .replace(/-+/g, "-")
+};
