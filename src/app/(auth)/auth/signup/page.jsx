64d3bf5fc4b230923d269716f5d44b8bf7e7804b
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-14 11:48:00 +0600

    feat: implement sign-up form and finalized sign-up page design

--- src/app/(auth)/auth/signup/page.jsx
+++ src/app/(auth)/auth/signup/page.jsx
@@ -1,7 +1,42 @@
+import { RiGoogleFill } from "@remixicon/react";
+
+import SignupForm from "@/components/ui/Form/SignupForm/SignupForm";
+import Logo from "@/components/ui/Logo/Logo";
+import Link from "next/link";
+
 const SignUpPage = () => {
     return (
-        <div className="w-full min-h-screen flex items-center justify-center">
-            <h1 className="text-white">Sign-Up Page</h1>
+        <div className="w-full min-h-screen flex items-center justify-center px-5">
+            <div className="w-full max-w-[390px]">
+                <div className="flex flex-col items-center gap-6 mb-5">
+                    <Logo />
+                    <div className="space-y-3 text-center">
+                        <h2 className="text-[28px] font-semibold text-[#ffffff] leading-tight">Start Learning Today!</h2>
+                        <p className="text-base font-no text-[#8e8e8e] leading-snug">Create your account and take the next step in your skill journey.</p>
+                    </div>
+                </div>
+                <div className="mt-20">
+                    <SignupForm />
+                </div>
+                <div className="w-full flex items-center justify-center gap-4 my-5">
+                    <div className="w-full h-px bg-[#2a2a2a]"></div>
+                    <span className="text-sm font-normal text-[#8e8e8e] uppercase">or</span>
+                    <div className="w-full h-px bg-[#2a2a2a]"></div>
+                </div>
+                <button className="w-full py-3 flex items-center justify-center gap-3 bg-transparent border border-[#2a2a2a] rounded-lg text-base font-medium text-[#ffffff] cursor-pointer transition-all duration-200 ease-in-out hover:bg-[#ffffff] hover:border-[#ffffff] hover:text-[#121212]">
+                    <RiGoogleFill size={22} />
+                    Continue with Google
+                </button>
+                <span className="text-sm font-medium text-[#8e8e8e] flex items-center justify-center gap-1 mt-5">
+                    Already have an account? 
+                    <Link 
+                        href={'/auth/login'}
+                        className="text-[#ffffff]"
+                    >
+                        Login.
+                    </Link>
+                </span>
+            </div>
         </div>
     );
 };
