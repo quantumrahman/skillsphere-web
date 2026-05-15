fc1510895de8f5974c48e5df632035cf76b59856
rakibulrahmandev <rakibulrahman.webdev@gmail.com>
2026-05-15 19:13:02 +0600

    feat: implement profile picture on header and change header codebase

--- src/components/shared/Header/Header.jsx
+++ src/components/shared/Header/Header.jsx
@@ -1,10 +1,26 @@
+"use client";
+
 import { RiMenu4Line } from "@remixicon/react";
+import { authClient, useSession } from "@/lib/auth-client";
+import { useRouter } from "next/navigation";
 
 import Link from "next/link";
 import Logo from "@/components/ui/Logo/Logo";
 import Navbar from "@/components/ui/Navbar/Navbar";
+import Image from "next/image";
 
 const Header = () => {
+    const router = useRouter();
+    const user = useSession();
+
+    const handleRedirectLogin = () => {
+        router.push('/auth/login');
+    };
+
+    const handleLogout = async () => {
+        const data =  await authClient.signOut();
+    };
+
     return (
         <div className="w-full px-5 py-5 bg-[#121212] border-b border-[#2a2a2a]">
             <div className="w-full max-w-[1400px] mx-auto">
@@ -14,20 +30,49 @@ const Header = () => {
                         <Navbar />
                     </div>
                     <div className="flex items-center justify-center gap-5 md:gap-8">
-                        <Link 
-                            href={'/auth/signup'}
-                            className="text-sm font-normal text-[#8e8e8e] hover:text-[#ffffff]"
-                        >
-                            Sign Up
-                        </Link>
-                        <button
-                            type="button"
-                            role="button"
-                            aria-label="button"
-                            className="px-4 py-2 rounded-full bg-[#ffffff] text-sm font-normal text-[#121212] cursor-pointer"
-                        >
-                            Login
-                        </button>
+                        {user?.data 
+                            ? (
+                                <>
+                                    <div className="w-8 h-8 rounded-full overflow-hidden flex items-center justify-center bg-[#2a2a2a]">
+                                        <div className="w-7 h-7 rounded-full relative">
+                                            <Image
+                                                src={'/images/jane_smith.png'}
+                                                alt="profile image"
+                                                fill={true}
+                                                className="object-cover w-full h-full rounded-full"
+                                            />
+                                        </div>
+                                    </div>
+                                    <button
+                                        onClick={handleLogout}
+                                        type="button"
+                                        role="button"
+                                        aria-label="button"
+                                        className="px-4 py-2 rounded-full bg-[#ffffff] text-sm font-normal text-[#121212] cursor-pointer"
+                                    >
+                                        Log Out
+                                    </button>
+                                </>
+                            ) : (
+                                <>
+                                    <Link 
+                                        href={'/auth/signup'}
+                                        className="text-sm font-normal text-[#8e8e8e] hover:text-[#ffffff]"
+                                    >
+                                        Sign Up
+                                    </Link>
+                                    <button
+                                        onClick={handleRedirectLogin}
+                                        type="button"
+                                        role="button"
+                                        aria-label="button"
+                                        className="px-4 py-2 rounded-full bg-[#ffffff] text-sm font-normal text-[#121212] cursor-pointer"
+                                    >
+                                        Login
+                                    </button>
+                                </>
+                            )
+                        }
                         <button 
                             type="button"
                             role="button"
